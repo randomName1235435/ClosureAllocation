ba929d46fab6dd1f58fcb261be7a36d0956c5d67
mm <unam3d@gmx.de>
2021-10-17 13:50:05 +0200

    Projektdateien hinzufügen.

---
+++ ClosureAllocation/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using BenchmarkDotNet.Running;
+
+namespace ClosureAllocation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<BenchmarkClass>();
+            Console.ReadLine();
+        }
+    }
+
+    [MemoryDiagnoser]
+    public class BenchmarkClass
+    {
+        [Benchmark]
+        public int WithClosure()
+        {
+            var sampleInt1 = 1;
+            var sampleInt2 = 2;
+            return new Func<int>(() => { return sampleInt1 + sampleInt2; })();
+        }
+        [Benchmark]
+        public int WithoutClosure()
+        {
+            return new Func<int>(() =>
+            {
+                var sampleInt1 = 1;
+                var sampleInt2 = 2;
+                return sampleInt1 + sampleInt2;
+            })();
+        }
+    }
+}
